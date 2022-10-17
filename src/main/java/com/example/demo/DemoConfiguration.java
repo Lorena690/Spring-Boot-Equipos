5bb306b3da954b4e144f65b9ec20ab36db18b488
Hector Ocampo <lorraines690.ls@gmail.com>
2022-10-17 14:58:10 -0500

    init project

---
+++ src/main/java/com/example/demo/DemoConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import com.example.demo.model.Liga;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DemoConfiguration {
+    @Bean
+    public Liga liga(){
+        return new Liga();
+    }
+}
